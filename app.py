5d73a7c9acc8b8b07c30fd488a348b24f934a4d1
Javohir <80480660+javohir-ergashboyev@users.noreply.github.com>
2024-01-12 21:38:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from fastai.vision.all import *
 import pathlib
 import plotly.express as px
+import platform
+plt=platform.system()
+if plt=='Linux': pathlib.Windows=pathlib.PosixPath
 temp=pathlib.PosixPath
 pathlib.PosixPath=pathlib.WindowsPath
 st.title('Transport classification model')
